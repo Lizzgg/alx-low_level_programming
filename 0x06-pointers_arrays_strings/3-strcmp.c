e9ec7812ece0eb80bd4b1399bda63abb67703709
Okoro Elizabeth Ezenwanyi <elizabethokoro5513@gmail.com>
2022-09-24 05:39:34 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,26 +1,17 @@
-#include "main.h" 
+#include "main.h"                                                                                                                                         
+                                                                                                                                                     
+/**                                                                                                                                                       
+*print_numbers - Print the numbers since 0 to 9                                                                                                           
+*
+*Return: The numbers since 0 up to 9                                                                                                                      
+*/                                                                                                                                                        
+void print_numbers(void)                                                                                                                                  
+{                                                                                                                                                         
+        int x;                                                                                                                                            
 
-/** 
- * _strncpy - Copies at most an inputted number 
- *            of bytes from string src into dest.
- * @dest: The buffer storing the string copy.     
- * @src: The source string.     
- * @n: The maximum number of bytes to copied from src.   
- * Return: A pointer to the resulting string dest.
- *         
- */
-char *_strncpy(char *dest, char *src, int n) 
-{ 
-	 int index = 0, src_len = 0; 
-
-	while (src[index++]) 
-		src_len++;
-
-	for(index = 0; src[index] && index < n; index++)
-		dest[index] = src[index]
-
-	for (index = src_len; index < n; index++)
-		dest[index] = '\0';  
-
-	return (dest); 
-}
+        for (x = 0; x <= 9; x++)                                                                                                                          
+        {                                                                                                                                                 
+                _putchar(x + '0');                                                                                                                        
+        }                                                                                                                                                 
+        _putchar('\n');                                                                                                                                   
+}                       
