56dee72830c3ba11c794cef26f30e4415521a64b
Okoro Elizabeth Ezenwanyi <elizabethokoro5513@gmail.com>
2022-09-24 09:15:35 -0700

    pointers,arrays and strings mandatory task

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,25 +1,3 @@
-Task 3.txt
-Who has access
-A
-System properties
-Type
-Text
-Size
-302 bytes
-Storage used
-302 bytes
-Location
-pointers arrays and strings
-Owner
-Antony Bahati
-Modified
-Sep 19, 2022 by Antony Bahati
-Opened
-9:42 PM by me
-Created
-Sep 19, 2022
-No description
-Viewers can download
 #include <stdio.h>
 #include "main.h"
 
