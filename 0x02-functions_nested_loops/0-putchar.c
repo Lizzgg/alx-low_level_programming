d3b14f23f03f3f3c8d5c3f643e2462785176db0a
Okoro Elizabeth Ezenwanyi <elizabethokoro5513@gmail.com>
2022-09-23 18:58:22 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
 #include "main.h" 
   
-/** 
- * main - print _putchar 
- * 
- * Return: zero 
- */ 
-int main(void) 
-{ 
-        char *main = "_putchar"; 
+ /** 
+  * main - prints _putchar, followed by a new line. 
+  * 
+  * Return: Always 0 (Success) 
+ **/ 
   
-        while (*main) 
-        { 
-                _putchar(*main); 
-                main++; 
-        } 
-        _putchar('\n'); 
-        return (0); 
-}
+ int main(void) 
+ { 
+         int i; 
+         char c[] = "_putchar"; 
+  
+         for (i = 0; i <= 7; i++) 
+         { 
+                 _putchar(c[i]); 
+         } 
+  
+         _putchar('\n'); 
+  
+         return (0); 
+ }
