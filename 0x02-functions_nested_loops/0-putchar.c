e1df7d403d3002964bea69d399b9dfd56bee1032
Okoro Elizabeth Ezenwanyi <elizabethokoro5513@gmail.com>
2022-09-24 10:00:59 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,23 +1,23 @@
-#include "main.h"
-
-/**
-* main - print putchar
-* Return: always 0
-*/
-
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-3 / 11
-
-return (0);
-
-}  
+#include "main.h" 
+  
+ /** 
+  * main - print _puchar 
+  * followed by a new line 
+  * Return: Always 0 
+  */ 
+  
+ int main(void) 
+ { 
+         _putchar('_'); 
+         _putchar('p'); 
+         _putchar('u'); 
+         _putchar('t'); 
+         _putchar('c'); 
+         _putchar('h'); 
+         _putchar('a'); 
+         _putchar('r'); 
+         _putchar('\n'); 
+  
+         return (0); 
+ } 
+   
