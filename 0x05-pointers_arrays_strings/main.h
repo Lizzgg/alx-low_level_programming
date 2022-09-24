585466b4b763dd3e17c4d3f3b0298ff9e47a7e18
Okoro Elizabeth Ezenwanyi <elizabethokoro5513@gmail.com>
2022-09-24 05:06:41 -0700

    arrays

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,23 @@
+0x05. C - Pointers, arrays and strings All mandatory and advanced task with quiz answer
+Quiz answers - 8 bytes, 8 bytes, &var, %p, Dereferencing, Yes
+
+,Segmentation fault, 98, 98, 402, 98, 20 bytes, *(arr + 2)
+
+
+
+
+First change the directory to - alx-low_level_programming/0x05-pointers_arrays_strings
+
+
+Use the command cd alx-low_level_programming/0x05-pointers_arrays_strings
+
+
+Then create a README.md using this command - echo “write any description you want here” > README.md
+
+
+After that type - cat > main.h and click enter then paste the following code
+
+
 #ifndef MAIN_H                                                                                                                        
 
 #define MAIN_H                                                                                                                        
@@ -38,3 +58,4 @@ int _atoi(char *str);
 
                                                                                                                                      
 
+#endif /*MAIN_H*/
