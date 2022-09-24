16d664555c176816bf789841c2452eaea4c3ca20
Okoro Elizabeth Ezenwanyi <elizabethokoro5513@gmail.com>
2022-09-24 06:35:40 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,14 @@
 #include "main.h"                                                                                  
+
 /**                                  
 *reset_to_98 - resets the value of given integer to 98            
 *@n: parameter                                   
-*Return : nothing                                 */                                      
+*Return : nothing                                 
+*/                                      
 
 void reset_to_98(int *n)
 {                                                 
-        *n = 98;                                  
+	*n = 98;                                  
 }
 
 
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,61 +1,21 @@
-0x05. C - Pointers, arrays and strings All mandatory and advanced task with quiz answer
-Quiz answers - 8 bytes, 8 bytes, &var, %p, Dereferencing, Yes
+#ifndef MAIN_H
+#define MAIN_H
 
-,Segmentation fault, 98, 98, 402, 98, 20 bytes, *(arr + 2)
+/**
+* void prototypes()
+* int prototypes()
+*/
 
-
-
-
-First change the directory to - alx-low_level_programming/0x05-pointers_arrays_strings
-
-
-Use the command cd alx-low_level_programming/0x05-pointers_arrays_strings
-
-
-Then create a README.md using this command - echo “write any description you want here” > README.md
-
-
-After that type - cat > main.h and click enter then paste the following code
-
-
-#ifndef MAIN_H                                                                                                                        
-
-#define MAIN_H                                                                                                                        
-
-                                                                                                                                     
-
-/**                                                                                                                                  
-
-* void prototypes()                                                                                                                  
-
-* int prototypes()                                                                                                                    
-
-*/                                                                                                                                    
-
-                                                                                                                                     
-
-void reset_to_98(int *n);                                                                                                            
-
-void swap_int(int *a, int *b);                                                                                                        
-
-int _strlen(char *s);                                                                                                                
-
-void _puts(char *str);                                                                                                                
-
-void print_rev(char *s);                                                                                                              
-
-void rev_string(char *s);                                                                                                            
-
-void puts2(char *str);                                                                                                                
-
-void puts_half(char *str);                                                                                                            
-
-void print_array(int *a, int n);                                                                                                      
-
-char *_strcpy(char *dest, char *src);                                                                                                
-
-int _atoi(char *str);                                                                                                                
-
-                                                                                                                                     
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *str);
 
 #endif /*MAIN_H*/
