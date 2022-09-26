04ff4f030a6ff16a0ca959e3d6446139ab041307
Okoro Elizabeth Ezenwanyi <elizabethokoro5513@gmail.com>
2022-09-26 13:15:20 -0700

    changes

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,12 @@ char *_strchr(char *s, char c)
 {
 
 	while (*s)
-	{
-		if (c ==*s)
+        }
+		if (c == *s)
 			return (s);
 		s++;
 	}
-	if (c ==*s)
+	if (c == *s)
 		return (s);
-	return (NULL);
-} 
+	return (NULL)
+}
