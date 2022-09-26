478ee424b4fb8b8a53ed6d92202fe9cebbd47f33
Okoro Elizabeth Ezenwanyi <elizabethokoro5513@gmail.com>
2022-09-26 15:22:55 -0700

    changes

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,35 +11,6 @@ char *_strstr(char *haystack, char *needle)
 {
 	char *result = haystack, *fneedle = needle;
 
-	themfon
-/
-alx-low_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-low_level_programming/0x07-pointers_arrays_strings/5-strstr.c
-@themfon
-themfon locate substring
- 1 contributor
-Executable File  32 lines (29 sloc)  435 Bytes
-#include "main.h"
-
-/**
-* _strstr - function locate
-*  @haystack: pointer to char
-*  @needle: pointer to char
-*  Return: 0
-*/
-
-char *_strstr(char *haystack, char *needle)
-{
-	char *result = haystack, *fneedle = needle;
-
 	while (*haystack)
 	{
 		while (*needle)
